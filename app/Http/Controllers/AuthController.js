d9b797ae37c7cd0add91d66bf5a6719a0d57541b
dalip123 <dalipkumar703@gmail.com>
2018-03-26 18:59:45 +0530

    oauth implementation half incomplete

--- app/Http/Controllers/AuthController.js
+++ app/Http/Controllers/AuthController.js
@@ -33,6 +33,70 @@ class AuthController {
     return
   }
 
+  * redirect (request, response) {
+   yield request.ally.driver('facebook').scope(['email']).redirect()
+ }
+
+ * handleCallback (request, response) {
+   const facebookUser = yield request.ally.driver('facebook').getUser()
+
+//user.getEmail()
+const user = new User();
+user.username = facebookUser.getName();
+user.email = facebookUser.getEmail();
+user.password ="123";
+
+yield user.save()
+//response.ok(facebookUser.getEmail());
+//response.route('test');
+const email = facebookUser.getEmail();
+const password = "123"
+
+const loginMessage = {
+  success: 'Logged-in Successfully!',
+  error: 'Invalid Credentials'
+}
+
+// Attempt to login with email and password
+const token = yield request.auth.attempt(email, password)
+console.log("token",token);
+if (token) {
+  let user = yield User.query().where('email', email).first()
+  user = user.toJSON()
+
+  delete user.created_at
+  delete user.updated_at
+
+  let access_token = token + request.input('client_secret')
+  response.cookie('access_token', access_token, {
+    httpOnly: true,
+    maxAge: '2592000',
+    path: '/'
+  })
+  response.cookie('sakshat', request.input('client_secret'), {
+    httpOnly: true,
+    maxAge: '2592000',
+    path: '/'
+  })
+ console.log("user:",user);
+  yield request.session.put('user', user)
+
+  yield request
+      .with({success: loginMessage.success})
+      .flash()
+  response.route('/profile')
+  return
+}
+
+yield request
+      .with({error: loginMessage.error})
+      .flash()
+response.redirect('back')
+
+   }
+
+
+
   * login (request, response) {
     const email = request.input('email')
     const password = request.input('password')
